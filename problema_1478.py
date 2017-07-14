6d36efedddaa2f37d711f4fdcb9249389e49552c
Welton Vaz de Souza <weltonvaz@gmail.com>
2017-07-14 13:16:53 -0300

    Create problema_1478.py
    atualização de 14/07/2017

---
+++ problema_1478.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from math import ceil
+user = -1
+
+while user != 0:
+
+    user = int(input())
+    while user < 0 or user > 100:
+        user = int(input())
+
+    if user >= 1:
+        matrix = [[(0) for row in range(user)] for column in range(user)]
+        ceiling = int(ceil(user/2.0))
+        for c in range(1, ceiling + 1):
+            for i in range(user):
+                for j in range(user):
+                    matrix[i][j] = abs(i-j) + 1
+
+    for i in matrix:
+        for item in i:
+            print('  %i' % item,end="")
+        print()
+    print()
